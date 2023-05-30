d6dc92011c3d268f1b5dc9db1f09f00458bec544
anhtuan200745 <tuannvats2208021@fpt.edu.vn>
2023-05-30 18:21:09 +0700

    gg

---
+++ database/migrations/2023_05_30_111747_create_news_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps('n_date');
+            $table->string('n_topic');
+            $table->string('n_title');
+            $table->string('n_content');
+            $table->string('n_image');
+            $table->string('n_intro');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+};
